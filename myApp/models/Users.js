5d0046cfcee9ecfe3d892f528142307978a212d7
GiancarloDevFS <giancarlo.barros@gmail.com>
2022-05-05 20:29:03 -0300

    criação da model Users

---
+++ myApp/models/Users.js
@@ -0,0 +1,58 @@
+const { Model, DataTypes } = require('sequelize');
+
+class user extends Model {
+    static init(connection) {
+        super.init({
+            id_pessoa: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true
+            },
+            nome: {
+                type: DataType.STRING(50),
+                allowNull: false
+            },
+            email: {
+                type: DataType.INTEGER(45),
+                allowNull: false
+            },
+            cpf: {
+                type: DataType.STRING(14),
+                allowNull: false
+            },
+            nascimento: {
+                type: DataType.DATE(/* pesquisar */),
+                allowNull: false
+            },
+            celular: {
+                type: DataType.INTEGER(11),
+                allowNull: false
+            },
+            senha: {
+                type: DataType.STRING(45),
+                allowNull: false
+            },
+            profissao: {
+                type: DataType.STRING(45),
+                allowNull: false
+            },
+            alergias: {
+                type: DataType.STRING(100),
+                allowNull: true
+            },
+            criancas: {
+                type: DataType.STRING(45),
+                allowNull: false
+            },
+            outros_pets: {
+                type: DataType.STRING(100),
+                allowNull: false
+            },
+
+        },{
+            sequelize: connection
+        })
+    }
+}
+
+module.exports = user;
