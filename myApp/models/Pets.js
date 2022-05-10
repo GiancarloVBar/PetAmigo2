8ef9a7ebb28ca05667ef6e027baa32744fbff0c5
marianafocassio <mariana.focassio@gmail.com>
2022-05-10 17:28:10 -0300

    refatoração model pets

--- myApp/models/Pets.js
+++ myApp/models/Pets.js
@@ -1,55 +1,55 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize')
 
-class pet extends Model {
-    static init(connection) {
-        super.init({
+class pets extends Model {
+   static init(connection) {
+      super.init(
+         {
             nome: {
-                type: DataTypes.STRING(45),
-                allowNull: false
+               type: DataTypes.STRING(45),
+               allowNull: false
             },
             idade: {
-                type: DataTypes.INTEGER(2),
-                allowNull: false
+               type: DataTypes.INTEGER(2),
+               allowNull: false
             },
             especie: {
-                type: DataTypes.STRING(45),
-                allowNull: false
+               type: DataTypes.STRING(45),
+               allowNull: false
             },
             raca: {
-                type: DataTypes.STRING(45),
-                allowNull: false
+               type: DataTypes.STRING(45),
+               allowNull: false
             },
             tamanho: {
-                type: DataTypes.STRING(45),
-                allowNull: false
+               type: DataTypes.STRING(45),
+               allowNull: false
             },
             cor: {
-                type: DataTypes.STRING(45),
-                allowNull: false
+               type: DataTypes.STRING(45),
+               allowNull: false
             },
             nascimento: {
-                type: DataTypes.DATE(/* verificar */),
-                allowNull: false
+               type: DataTypes.DATE(),
+               allowNull: false
             },
             sexo: {
-                type: DataTypes.STRING(1),
-                allowNull: true
+               type: DataTypes.STRING(1),
+               allowNull: true
             }
-        },{
-            sequelize: connection
-        })
-    }
+         },
+         {
+            sequelize: connection,
+            freezeTableName: true
+         }
+      )
+   }
 
-    static associate(models) {
-        this.belongsTo(model.user, {
-            foreignKey: "user_id",
-            as: "pet_user"
-        }),
-        this.belongsTo(model.ong, {
-            foreignKey: "ong_id",
-            as: "pet_ong"
-        })
-    }
+   static associate(models) {
+      this.belongsTo(models.ongs, {
+         foreignKey: 'ong_id',
+         as: 'ong_pets'
+      })
+   }
 }
 
-module.exports = pet;
+module.exports = pets
