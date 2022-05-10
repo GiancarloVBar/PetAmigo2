a20405f598765bd68fe5091f2c7377d8671f366f
alvaro <alvarocondeorlando@gmail.com>
2022-05-10 17:38:43 -0300

    alterando configuração index.js database

--- myApp/src/database/index.js
+++ myApp/src/database/index.js
@@ -4,13 +4,21 @@ const connection = new Sequelize(dbConfig);
 
 const User = require('../../models/Users');
 const UsersAddresses = require('../../models/UsersAddresses')
+const Pets = require('../../models/Pets')
+const Ongs = require('../../models/Ongs')
+const OngsAddresses = require('../../models/OngsAddresses')
 
-
+Ongs.init(connection)
+Pets.init(connection)
 User.init(connection);
+OngsAddresses.init(connection)
 UsersAddresses.init(connection);
 
 User.associate(connection.models)
 UsersAddresses.associate(connection.models)
+Ongs.associate(connection.models)
+OngsAddresses.associate(connection.models)
+Pets.associate(connection.models)
 
 
 module.exports = connection;
