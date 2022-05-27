a0bd99beb6712ecfcd27db8272fe2a1dd0436099
marianafocassio <mariana.focassio@gmail.com>
2022-05-27 01:21:49 -0300

    arrumando bug do include header

--- myApp/controllers/SearchController.js
+++ myApp/controllers/SearchController.js
@@ -1,12 +1,24 @@
+const  { siglas, nomeEstados, menuTamanho, menuPet }  = require('../public/javascripts/info')
+
 const SearchController = {
    searchView: (req, res) => {
       res.render('search', {
-         title: 'Search - PetAmigo'
+         title: 'Search - PetAmigo',
+         menuPet,
+         menuTamanho,
+         siglas,
+         nomeEstados,
+         usuario: req.session.usuario,
+         ong: req.session.ong
       })
    },
 
    verMaisView: (req, res) => {
-      res.render('verMais', { title: 'Ver Mais' })
+      res.render('verMais', {
+         title: 'Ver Mais',
+         usuario: req.session.usuario,
+         ong: req.session.ong
+      })
    }
 }
 
