1cef48fc623d25909463137913c416f56ad28028
GiancarloDevFS <giancarlo.barros@gmail.com>
2022-05-26 21:48:01 -0300

    ajustando view signupong, criando onFormValidator

--- myApp/middlewares/ongFormValidator.js
+++ myApp/middlewares/ongFormValidator.js
@@ -29,12 +29,9 @@ const ongValidateRegister = [
     .isLength({ min: 14, max: 14 })
     .withMessage('CNPJ inválido!'),
 
-    body('telefone')
+    body('telefone')/*  */
     .notEmpty()
-    .withMessage('Preencha esse campo!')
-    .bail()
-    .isLength({ min: 3, max: 50 })
-    .withMessage('O nome deve ter entre 3 e 50 caracteres!'),
+    .withMessage('Preencha esse campo!'),
 
     body('senha')/*  */
     .notEmpty()
@@ -43,82 +40,88 @@ const ongValidateRegister = [
     .isLength({ min: 6, max: 12 })
     .withMessage('A senha deve ter entre 6 e 12 caracteres!'),
 
-    body('anos_de_funcionamento')
+    body('anos_de_funcionamento')/*  */
     .notEmpty()
     .withMessage('Preencha esse campo!')
     .bail()
-    .isLength({ min: 3, max: 50 })
-    .withMessage('O nome deve ter entre 3 e 50 caracteres!'),
+    .isLength({ min: 1, max: 2 })
+    .withMessage('Não pode ter mais que 2 caracteres')
+    .bail()
+    .isInt(),
 
-    body('quantidade_animais')
+    body('quantidade_animais')/*  */
     .notEmpty()
     .withMessage('Preencha esse campo!')
     .bail()
-    .isLength({ min: 3, max: 50 })
-    .withMessage('O nome deve ter entre 3 e 50 caracteres!'),
+    .isLength({ min: 1, max: 4 })
+    .withMessage('Não pode ter mais que 4 caracteres!')
+    .bail()
+    .isInt(),
 
-    body('historia')
+    body('historia')/*  */
     .notEmpty()
     .withMessage('Preencha esse campo!')
     .bail()
-    .isLength({ min: 3, max: 50 })
-    .withMessage('O nome deve ter entre 3 e 50 caracteres!'),
+    .isLength({ min: 3, max: 1234 })
+    .withMessage('Conte uma breve história!'),
 
-    body('cep')
+    body('cep')/*  */
     .notEmpty()
     .withMessage('Preencha esse campo!')
     .bail()
-    .isLength({ min: 3, max: 50 })
-    .withMessage('O nome deve ter entre 3 e 50 caracteres!'),
+    .isLength({ min: 8, max: 8 })
+    .bail()
+    .isInt()
+    .withMessage('CEP inválido!'),
 
-    body('endereco')
+    body('endereco')/*  */
     .notEmpty()
     .withMessage('Preencha esse campo!')
     .bail()
-    .isLength({ min: 3, max: 50 })
-    .withMessage('O nome deve ter entre 3 e 50 caracteres!'),
+    .isLength({ min: 3, max: 100 })
+    .withMessage('O nome deve ter entre 3 e 100 caracteres!'),
 
-    body('numero')
+    body('numero')/*  */
     .notEmpty()
     .withMessage('Preencha esse campo!')
     .bail()
-    .isLength({ min: 3, max: 50 })
-    .withMessage('O nome deve ter entre 3 e 50 caracteres!'),
+    .isLength({ min: 1, max: 5 })
+    .withMessage('O nome deve ter entre 1 e 5 caracteres!'),
 
-    body('complemento')
+    body('complemento')/*  */
     .notEmpty()
     .withMessage('Preencha esse campo!')
     .bail()
-    .isLength({ min: 3, max: 50 })
-    .withMessage('O nome deve ter entre 3 e 50 caracteres!'),
+    .isLength({ min: 3, max: 45 })
+    .withMessage('O nome deve ter entre 3 e 45 caracteres!'),
 
-    body('bairro')
+    body('bairro')/*  */
     .notEmpty()
     .withMessage('Preencha esse campo!')
     .bail()
-    .isLength({ min: 3, max: 50 })
-    .withMessage('O nome deve ter entre 3 e 50 caracteres!'),
+    .isLength({ min: 3, max: 45 })
+    .withMessage('O nome deve ter entre 3 e 45 caracteres!'),
 
-    body('cidade')
+    body('cidade')/*  */
     .notEmpty()
     .withMessage('Preencha esse campo!')
     .bail()
-    .isLength({ min: 3, max: 50 })
-    .withMessage('O nome deve ter entre 3 e 50 caracteres!'),
+    .isLength({ min: 3, max: 45 })
+    .withMessage('O nome deve ter entre 3 e 45 caracteres!'),
 
-    body('estado')
+    body('estado')/*  */
     .notEmpty()
     .withMessage('Preencha esse campo!')
     .bail()
-    .isLength({ min: 3, max: 50 })
-    .withMessage('O nome deve ter entre 3 e 50 caracteres!'),
+    .isLength({ min: 2, max: 2 })
+    .withMessage('Forneça o Estado onde está a ONG!'),
 
-    body('tipo_local')
+    body('tipo_local')/*  */
     .notEmpty()
     .withMessage('Preencha esse campo!')
     .bail()
-    .isLength({ min: 3, max: 50 })
-    .withMessage('O nome deve ter entre 3 e 50 caracteres!'),
+    .isLength({ min: 3, max: 45 })
+    .withMessage('O nome deve ter entre 3 e 45 caracteres!'),
 
 
 ]
