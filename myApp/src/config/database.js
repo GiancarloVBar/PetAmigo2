19641efa55df9ba3bca474f257b7e0abda23cc03
Micheli Ferreira <micheliferreira846@gmail.com>
2022-05-27 22:30:36 -0300

    Adicionado foto na tabela pets

--- myApp/src/config/database.js
+++ myApp/src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     dialect: 'mysql',
     host:'localhost',
     username:'root',
-    password:'Bund@s2134',
+    password:'admin',
     database:'petAmigo',
     define:{
         timestamps: true,
